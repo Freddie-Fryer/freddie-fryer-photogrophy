24bbab1a1081519ed19ea847de469cc6e0e953be
Freddie Fryer <freddiefryercode@gmail.com>
2021-04-08 15:29:14 +0100

    added images controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'images/index'
+  get 'images/show'
+  get 'images/new'
+  get 'images/create'
+  get 'images/edit'
+  get 'images/update'
+  get 'images/destroy'
+  get 'index/show'
+  get 'index/new'
+  get 'index/create'
+  get 'index/edit'
+  get 'index/update'
+  get 'index/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
