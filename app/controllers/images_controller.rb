b2008327f8593d0b98cf72663e4f766f63840a15
Freddie Fryer <freddiefryercode@gmail.com>
2021-04-09 14:16:17 +0100

    small text change in images controller

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,5 @@
 class ImagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show, ]
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
   end
 
